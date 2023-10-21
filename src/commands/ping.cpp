46d532d15283e7ce0988034abc0e708437a3c9a0
Craig Edwards <craigedwards@brainbox.cc>
2023-10-21 10:56:14 +0000

    reword and get rid of error check

--- src/commands/ping.cpp
+++ src/commands/ping.cpp
@@ -24,21 +24,14 @@ void ping_command::route(const dpp::slashcommand_t &event)
 	/* Get the rest ping from discord. */
 	double discord_api_ping = bot->rest_ping * 1000;
 
-	embed.add_field("Rest ping", std::to_string(discord_api_ping), true);
+	embed.add_field("REST Ping", std::to_string(discord_api_ping) + "ms", true);
 
 	/* Calculate the DB time */
 	double start = dpp::utility::time_f();
 	db::resultset q = db::query("SHOW TABLES", {});
 
-	/* We don't ever want to show ping if the db failed to connect.
-	 * To make sure of this, we'll make sure the error field is empty, if it isn't then we'll state that the db failed.
-	 */
-	if(!db::error().empty()) {
-		embed.add_field("DB ping", ":warning: Failed to communicate with database. Report this ASAP.", true);
-	} else {
-		double db_ping = (dpp::utility::time_f() - start) * 1000;
-		embed.add_field("DB ping", std::to_string(db_ping), true);
-	}
+	double db_ping = (dpp::utility::time_f() - start) * 1000;
+	embed.add_field("Database Ping", std::to_string(db_ping) + "ms", true);
 
 	event.reply(dpp::message().add_embed(embed));
 }
