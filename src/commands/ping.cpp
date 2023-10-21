53ff2db8ef677902fe059cd2f7eb7fe7d055f85e
Craig Edwards <craigedwards@brainbox.cc>
2023-10-21 11:44:27 +0000

    format decimals

--- src/commands/ping.cpp
+++ src/commands/ping.cpp
@@ -2,6 +2,7 @@
 #include <beholder/database.h>
 #include <beholder/commands/ping.h>
 #include <beholder/database.h>
+#include <fmt/format.h>
 
 dpp::slashcommand ping_command::register_command(dpp::cluster& bot)
 {
@@ -24,14 +25,14 @@ void ping_command::route(const dpp::slashcommand_t &event)
 	/* Get the rest ping from discord. */
 	double discord_api_ping = bot->rest_ping * 1000;
 
-	embed.add_field("REST Ping", std::to_string(discord_api_ping) + "ms", true);
+	embed.add_field("REST Ping", fmt::format("{:.02f}ms", discord_api_ping), true);
 
 	/* Calculate the DB time */
 	double start = dpp::utility::time_f();
-	db::resultset q = db::query("SHOW TABLES", {});
+	db::resultset q = db::query("SHOW TABLES");
 
 	double db_ping = (dpp::utility::time_f() - start) * 1000;
-	embed.add_field("Database Ping", std::to_string(db_ping) + "ms", true);
+	embed.add_field("Database Ping", fmt::format("{:.02f}ms", db_ping), true);
 
 	event.reply(dpp::message().add_embed(embed));
 }
