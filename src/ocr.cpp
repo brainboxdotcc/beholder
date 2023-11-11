61ddf5a1e9a5dcb182d5983c961e20a1373c2240
Craig Edwards <craigedwards@brainbox.cc>
2023-11-11 21:35:56 +0000

    make caching much simpler by splitting up into separate scan_cache and api_cache tables

--- src/ocr.cpp
+++ src/ocr.cpp
@@ -14,7 +14,6 @@ namespace ocr {
 
 	void image(std::string file_content, const dpp::attachment attach, dpp::cluster& bot, const dpp::message_create_t ev) {
 		std::string ocr;
-
 		dpp::utility::set_thread_name("img-scan/" + std::to_string(concurrent_images));
 
 		on_thread_exit([&bot]() {
@@ -26,18 +25,27 @@ namespace ocr {
 
 		/* This loop is not redundant - it ensures the spawn class is scoped */
 		try {
-			const char* const argv[] = {"./tessd", nullptr};
-			spawn tessd(argv);
-			bot.log(dpp::ll_info, fmt::format("spawned tessd; pid={}", tessd.get_pid()));
-			tessd.stdin.write(file_content.data(), file_content.length());
-			tessd.send_eof();
-			std::string l;
-			while (std::getline(tessd.stdout, l)) {
-				ocr.append(l).append("\n");
-			}
-			int ret = tessd.wait();
-			if (ret > static_cast<int>(tessd::exit_code::no_error) - 1 && ret < static_cast<int>(tessd::exit_code::max)) {
-				bot.log(ret ? dpp::ll_error : dpp::ll_info, fmt::format("tessd status {}: {}", ret, tessd::tessd_error[ret]));
+			db::resultset rs = db::query("SELECT * FROM scan_cache WHERE hash = '?'", { hash });
+			if (!rs.empty()) {
+				ocr = rs[0].at("ocr");
+				bot.log(dpp::ll_info, fmt::format("image {} is in OCR cache", hash));
+			} else {
+				const char* const argv[] = {"./tessd", nullptr};
+				spawn tessd(argv);
+				bot.log(dpp::ll_info, fmt::format("spawned tessd; pid={}", tessd.get_pid()));
+				tessd.stdin.write(file_content.data(), file_content.length());
+				tessd.send_eof();
+				std::string l;
+				while (std::getline(tessd.stdout, l)) {
+					ocr.append(l).append("\n");
+				}
+				int ret = tessd.wait();
+				if (ret > static_cast<int>(tessd::exit_code::no_error) - 1 && ret < static_cast<int>(tessd::exit_code::max)) {
+					bot.log(ret ? dpp::ll_error : dpp::ll_info, fmt::format("tessd status {}: {}", ret, tessd::tessd_error[ret]));
+				}
+				if (!ocr.empty()) {
+					db::query("INSERT INTO scan_cache (hash, ocr) VALUES('?','?') ON DUPLICATE KEY UPDATE ocr = '?'", { hash, ocr, ocr });
+				}
 			}
 		} catch (const std::runtime_error& e) {
 			bot.log(dpp::ll_error, e.what());
@@ -54,7 +62,6 @@ namespace ocr {
 					std::string pattern_wild = "*" + p + "*";
 					if (line.length() && p.length() && match(line.c_str(), pattern_wild.c_str())) {
 						delete_message_and_warn(file_content, bot, ev, attach, p, false);
-						db::query("INSERT INTO scan_cache (hash, ocr) VALUES('?','?') ON DUPLICATE KEY UPDATE ocr = '?'", { hash, ocr, ocr });
 						return;
 					}
 				}
@@ -81,7 +88,6 @@ namespace ocr {
 				for (size_t x = 0; x < file_content.length() - 3; ++x) {
 					if (filebits[x] == 0x21 && filebits[x + 1] == 0xF9 && filebits[x + 2] == 0x04) {
 						bot.log(dpp::ll_debug, "Detected animated gif, name: " + attach.filename + "; not scanning with IR");
-						db::query("INSERT INTO scan_cache (hash, ocr) VALUES('?','?') ON DUPLICATE KEY UPDATE ocr = '?'", { hash, ocr, ocr });
 						return;
 					}
 				}
@@ -93,45 +99,47 @@ namespace ocr {
 			std::string active_models = m[0].at("selected");
 			db::query("UPDATE guild_config SET calls_this_month = calls_this_month + 1 WHERE guild_id = ?", {ev.msg.guild_id.str() });
 
-			/* Make API request, upload the image, don't get the API to download it.
-			 * This is more expensive for us in terms of bandwidth, but we are going
-			 * to be able to check more images more of the time this way. We already
-			 * have the image data in memory and can upload it straight to the API.
-			 * Asking the API endpoint to download it via URL risks them being rate
-			 * limited or blocked as they will be making many hundreds of requests
-			 * per minute and we will not.
-			 */
-			httplib::Client cli(endpoint.c_str());
-			cli.enable_server_certificate_verification(false);
-			httplib::MultipartFormDataItems items = {
-				{ fields[4], file_content, attach.filename, "application/octet-stream" },
-				{ fields[1], irconf["credentials"]["username"], "", "" },
-				{ fields[2], irconf["credentials"]["password"], "", "" },
-				{ fields[0], active_models, "", "" },
-			};
-			auto res = cli.Post(irconf["path"].get<std::string>().c_str(), items);
-
-			if (res) {
-				if (res->status < 400) {
-					json answer;
-					try {
-						answer = json::parse(res->body);
-					} catch (const std::exception& e) {
-						sentry::log_catch(typeid(e).name(), e.what());
-					}
-					find_banned_type(answer, attach, bot, ev, file_content);
-					db::query("INSERT INTO scan_cache (hash, ocr, api) VALUES('?','?','?') ON DUPLICATE KEY UPDATE ocr = '?', api = '?'", { hash, ocr, res->body, ocr, res->body });
-				} else {
-					bot.log(dpp::ll_warning, "API Error: '" + res->body + "' status: " + std::to_string(res->status));
-					db::query("INSERT INTO scan_cache (hash, ocr) VALUES('?','?') ON DUPLICATE KEY UPDATE ocr = '?'", { hash, ocr, ocr });
-				}
+			json answer;
+			db::resultset rs = db::query("SELECT * FROM api_cache WHERE hash = '?'", { hash });
+			if (!rs.empty()) {
+				bot.log(dpp::ll_info, fmt::format("image {} is in API cache", hash));
+				answer = json::parse(rs[0].at("api"));
+				find_banned_type(answer, attach, bot, ev, file_content);
 			} else {
-				auto err = res.error();
-				bot.log(dpp::ll_warning, fmt::format("API Error: {}", httplib::to_string(err)));
-				db::query("INSERT INTO scan_cache (hash, ocr) VALUES('?','?') ON DUPLICATE KEY UPDATE ocr = '?'", { hash, ocr, ocr });
+				/* Make API request, upload the image, don't get the API to download it.
+				* This is more expensive for us in terms of bandwidth, but we are going
+				* to be able to check more images more of the time this way. We already
+				* have the image data in memory and can upload it straight to the API.
+				* Asking the API endpoint to download it via URL risks them being rate
+				* limited or blocked as they will be making many hundreds of requests
+				* per minute and we will not.
+				*/
+				httplib::Client cli(endpoint.c_str());
+				cli.enable_server_certificate_verification(false);
+				httplib::MultipartFormDataItems items = {
+					{ fields[4], file_content, attach.filename, "application/octet-stream" },
+					{ fields[1], irconf["credentials"]["username"], "", "" },
+					{ fields[2], irconf["credentials"]["password"], "", "" },
+					{ fields[0], active_models, "", "" },
+				};
+				auto res = cli.Post(irconf["path"].get<std::string>().c_str(), items);
+
+				if (res) {
+					if (res->status < 400) {
+						try {
+							answer = json::parse(res->body);
+						} catch (const std::exception& e) {
+						}
+						find_banned_type(answer, attach, bot, ev, file_content);
+						db::query("INSERT INTO api_cache (hash, api) VALUES('?','?') ON DUPLICATE KEY UPDATE api = '?'", { hash, res->body, res->body });
+					} else {
+						bot.log(dpp::ll_warning, "API Error: '" + res->body + "' status: " + std::to_string(res->status));
+					}
+				} else {
+					auto err = res.error();
+					bot.log(dpp::ll_warning, fmt::format("API Error: {}", httplib::to_string(err)));
+				}
 			}
-		} else {
-			db::query("INSERT INTO scan_cache (hash, ocr) VALUES('?','?') ON DUPLICATE KEY UPDATE ocr = '?'", { hash, ocr, ocr });
 		}
 	}
 
